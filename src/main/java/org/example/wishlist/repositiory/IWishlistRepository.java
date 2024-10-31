f3ce87472380cdcfa9f1b730a7ed2ec1574fbf5c
amaliekea <amje0001@stud.kea.dk>
2024-10-31 10:55:47 +0100

    opsætning af model classes og interface

--- src/main/java/org/example/wishlist/repositiory/IWishlistRepository.java
+++ src/main/java/org/example/wishlist/repositiory/IWishlistRepository.java
@@ -1,4 +1,23 @@
 package org.example.wishlist.repositiory;
+import org.example.wishlist.model.Tag;
+import org.example.wishlist.model.Wish;
+import java.util.List;
 
 public interface IWishlistRepository {
+
+    //CREATE
+    void addwish(Wish wish);
+
+    //READ
+    List<Wish> getAllWishes();
+
+    List<Tag> getAvaliableTags();
+
+    List<Tag> getTags(Wish wish);
+
+    //UPDATE
+    void updateWish(Wish wish);
+
+    //DELETE
+    void deleteWish(int id);
 }
