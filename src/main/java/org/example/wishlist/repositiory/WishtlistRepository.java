504d6bc431799e89132ed803c1db2a543aa8ccb5
amaliekea <amje0001@stud.kea.dk>
2024-10-31 11:14:17 +0100

    opsætning af model classes og interface

--- src/main/java/org/example/wishlist/repositiory/WishtlistRepository.java
+++ src/main/java/org/example/wishlist/repositiory/WishtlistRepository.java
@@ -1,4 +1,40 @@
 package org.example.wishlist.repositiory;
 
-public class WishtlistRepository {
+import org.example.wishlist.model.Tag;
+import org.example.wishlist.model.Wish;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("DEPARTMENT_REPOSITORY")
+public class WishtlistRepository implements IWishlistRepository {
+    @Override
+    public void addwish(Wish wish) {
+
+    }
+
+    @Override
+    public List<Wish> getAllWishes() {
+        return List.of();
+    }
+
+    @Override
+    public List<Tag> getAvaliableTags() {
+        return List.of();
+    }
+
+    @Override
+    public List<Tag> getTags(Wish wish) {
+        return List.of();
+    }
+
+    @Override
+    public void updateWish(Wish wish) {
+
+    }
+
+    @Override
+    public void deleteWish(int id) {
+
+    }
 }
