e8556a768ff2563084bbed0e5ee0b05ee4ba478c
amaliekea <amje0001@stud.kea.dk>
2024-10-31 21:39:20 +0100

    mavenaddition i yml

--- src/main/java/org/example/wishlist/repositiory/WishtlistRepository.java
+++ src/main/java/org/example/wishlist/repositiory/WishtlistRepository.java
@@ -12,7 +12,7 @@ import java.sql.Connection;
 import java.util.List;
 import java.sql.*;
 
-//h
+
 @Repository("DEPARTMENT_REPOSITORY")
 @Lazy
 public class WishtlistRepository implements IWishlistRepository {
