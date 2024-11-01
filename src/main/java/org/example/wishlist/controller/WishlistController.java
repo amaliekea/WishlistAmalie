cfbe4cf736f03ec4e129f55c4c7541819388cb77
malthe-kea <mati0002@stud.kea.dk>
2024-11-01 12:12:13 +0100

    De fleste templates oprettet.

--- src/main/java/org/example/wishlist/controller/WishlistController.java
+++ src/main/java/org/example/wishlist/controller/WishlistController.java
@@ -1,10 +1,15 @@
 package org.example.wishlist.controller;
 
+import org.example.wishlist.model.Wish;
 import org.example.wishlist.service.WishlistService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -16,10 +21,27 @@ public class WishlistController {
         this.wishlistService = wishlistService;
     }
 
+    @GetMapping("/")
+    public String showAllWishes(Model model) {
+        List<Wish> wishes = wishlistService.getAllWishes();
+        //
+        return "show-wishlist";
+    }
+
+    @PostMapping("attractions/delete")
+    public String getWishToDelete(@RequestParam String name) {
+        //TODO kald den rigtige gettermetoder, når den er blevet lavet.
+        //Wish w = wishlistService.getWishByName(name);
+        //if (w != null) {
+        //    wishlistService.deleteDTOWishlistItem(w);
+        //}
+        return "redirect:/";
+    }
+
     @PostMapping("/delete")
     public String deleteWishlistItem(@RequestParam int wish_Id) {
         wishlistService.deleteDTOaWishlistItem(wish_Id);
         //TODO indsæt korrekt redirection herunder.
-        return "redirect:/";
+        return "redirect:/show-wishlist";
     }
 }
